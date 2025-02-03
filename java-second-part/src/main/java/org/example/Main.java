94631d8111b576c9b1440fbdb7172068e32f3e98
Irina Khromova <irina.khromova@jetbrains.com>
2025-02-03 09:29:39 +0200

    Add 2 new modules

---
+++ java-second-part/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import static java.lang.System.out;
+import static java.lang.System.err;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+    public static void newFunction2() {
+        if (true) {}
+    }
+}
